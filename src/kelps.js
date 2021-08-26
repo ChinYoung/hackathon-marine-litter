c8fb426db1fd0738d08ff04b0316cdc3de4dea7a
leo.cai <leo_cai@epam.com>
2021-08-26 10:22:14 +0800

    commit kelps

---
+++ src/kelps.js
@@ -0,0 +1,63 @@
+class Kelp {
+  constructor({x, y, height, width = 15, ctx}) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.ctx = ctx;
+    this.color = '#37524b';
+    this.alpha = 0.7;
+    this.deltaTime = 0;
+    this.quadraticEndX = 0;
+    this.quadraticEndY = 0;
+  }
+
+  draw() {
+    let {x, ctx, height, width, color, alpha} = this;
+    this.deltaTime += 0.01;
+    let sin = Math.sin(this.deltaTime);
+    this.quadraticEndX = x + sin * 70;
+    this.quadraticEndY = (ctx.canvas.height - height) + Math.abs(sin * 8);
+    ctx.save();
+    ctx.globalAlpha = alpha;
+    ctx.lineWidth = width;
+    ctx.strokeStyle = color;
+    ctx.lineCap = "round";
+    ctx.beginPath();
+    ctx.moveTo(x, ctx.canvas.height);
+    ctx.quadraticCurveTo(x, (ctx.canvas.height - height) * 1.18, this.quadraticEndX, this.quadraticEndY);
+    ctx.stroke(); // 进行绘制
+    ctx.closePath();
+    ctx.restore();
+  }
+}
+
+class Kelps {
+  
+  init(ctx, { canvasWidth, canvasHeight}) {
+    this.ctx = ctx;
+    this.kelpList = [];
+    this.kelpNum = 60;
+    let gap = -50;
+    for (let i = 0; i < this.kelpNum; i++) {
+      let x = gap + (Math.random() * 1.2 + 0.3) * 15;
+      let kelp = new Kelp({
+        x,
+        y: canvasHeight,
+        height: 120 + Math.random() * 100,
+        ctx: ctx
+      })
+      gap = x;
+      this.kelpList.push(kelp);
+    }
+  }
+
+  draw() {
+    const { ctx } = this;
+    for (let i = 0; i < this.kelpList.length; i++) {
+      this.kelpList[i].draw();
+    }
+  }
+}
+
+export default Kelps;
